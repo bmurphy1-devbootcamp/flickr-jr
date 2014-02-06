f178aea40c56379e4b8c5a62c0e4f2af3e6875ae
Brian Murphy <bmurphy1@mac.com>
2014-02-06 16:07:19 -0600

    Add required code to get carrierwave working

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ require 'carrierwave'
 class Photo < ActiveRecord::Base
   belongs_to :album
   belongs_to :user
-  include ImageUploader
-  uploader = ImageUploader.new
-  mount_uploader :image, uploader
+
+  # uploader = ImageUploader.new
+  mount_uploader :image, ImageUploader
 end
